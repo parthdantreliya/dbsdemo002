0a69fe6a77900e3dde4cdea15a97e3471d1837c7
parthdantreliya <32670878+parthdantreliya@users.noreply.github.com>
2017-10-10 13:34:06 +0100

    Create peopleapi.php

---
+++ peopleapi.php
@@ -0,0 +1,20 @@
+<?php
+class $person
+{
+var $personID;
+var $firstname;
+var $lastname;
+}
+$items= array();
+for($i=0;$i<10;$i++)
+{
+$personrecord = $new person();
+$personrecord->personID= "$i"; 
+$personrecord->firstname= "Name $i";
+$personrecord->firstname="LastName $i";
+$items[i]= $personrecord;
+}
+header('content-type: application/json');
+$jsonOutput= json_encode($items);
+echo $jsonOutput;
+?>
