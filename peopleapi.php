e877fa0dd87607def3236840410b45bc69861810
parthdantreliya <32670878+parthdantreliya@users.noreply.github.com>
2017-10-10 13:45:21 +0100

    Update peopleapi.php

--- peopleapi.php
+++ peopleapi.php
@@ -8,13 +8,13 @@ var $lastname;
 $items= array();
 for($i=0;$i<10;$i++)
 {
-$personrecord = $new person();
+$personrecord = new person();
 $personrecord->personID= $i; 
 $personrecord->firstname= "Name $i";
 $personrecord->firstname="LastName $i";
 $items[i]= $personrecord;
 }
-header('content-type: application/json');
+header('Content-Type: application/json');
 $jsonOutput= json_encode($items);
 echo $jsonOutput;
 ?>
