63230991d2aecffaaf3d23292304082f2e55f0d6
parthdantreliya <32670878+parthdantreliya@users.noreply.github.com>
2017-10-10 13:38:45 +0100

    Update peopleapi.php

--- peopleapi.php
+++ peopleapi.php
@@ -9,7 +9,7 @@ $items= array();
 for($i=0;$i<10;$i++)
 {
 $personrecord = $new person();
-$personrecord->personID= "$i"; 
+$personrecord->personID= $i; 
 $personrecord->firstname= "Name $i";
 $personrecord->firstname="LastName $i";
 $items[i]= $personrecord;
